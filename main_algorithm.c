5a521617724c442473f3d97fdd14304ee8f8ed9e
max_dmitry <dmitrymarach2000@yandex.ru>
2019-12-13 16:57:15 +0300

    basic_handler

---
+++ main_algorithm.c
@@ -0,0 +1,70 @@
+#include <open62541.h>
+
+void write_detector_value(UA_UInt16 value) {
+	
+	static char cycle = 0;
+	UA_UInt16 ref;
+
+	switch(cycle){
+		case 0:{
+			ref = 0x1801;
+			ref ^= value;
+			if(ref == 0){
+				cycle++;
+			}else{
+				cycle = 0;
+				for(int i = 0; i < 16; ++i){
+					ref%2 == 1 ? /*EVENT*/: ;
+					ref >>= 1;
+				}
+			}
+		}
+			break;
+		case 1:{
+			ref = 0x2116;
+			ref ^= value;
+			if(ref == 0){
+				cycle++;
+			}else{
+				cycle = 0;
+				for(int i = 0; i < 16; ++i){
+					ref%2 == 1 ? : ;
+					ref >>= 1;
+				}
+				/*EVENT*/
+			}
+		}
+			break;
+		case 2:{
+			ref = 0x5125;
+			ref ^= value;
+			if(ref == 0){
+				cycle++;
+			}else{
+				cycle = 0;
+				for(int i = 0; i < 16; ++i){
+					ref%2 == 1 ? : ;
+					ref >>= 1;
+				}
+				/*EVENT*/
+			}
+		}
+			break;
+		case 3:{
+			ref = 0x2800;
+			ref ^= value;
+			if(ref == 0){
+				cycle = 0;
+				/*EVENT_OK*/
+			}else{
+				cycle = 0;
+				for(int i = 0; i < 16; ++i){
+					ref%2 == 1 ? : ;
+					ref >>= 1;
+				}
+				/*EVENT*/
+			}
+		}
+			break;
+	}
+}
